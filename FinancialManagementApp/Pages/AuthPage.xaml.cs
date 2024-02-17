b49f2c4ada6221a578ac5638dbe381457567011f
TankistPro <nakol.nayk@mail.ru>
2024-02-17 18:30:54 +0300

    Добавлены страницы и переходы между ними

--- FinancialManagementApp/Pages/AuthPage.xaml.cs
+++ FinancialManagementApp/Pages/AuthPage.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using FinancialManagementApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FinancialManagementApp
 {
@@ -23,6 +17,16 @@ namespace FinancialManagementApp
         public AuthPage()
         {
             InitializeComponent();
+        }
+
+        private void GoToRegistration(object sender, RoutedEventArgs e)
+        {
+           Application.Current.MainWindow.Content = new RegistrationPage();
+        }
+
+        private void AuthUser(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Content = new HomePage();
         }
     }
 }
