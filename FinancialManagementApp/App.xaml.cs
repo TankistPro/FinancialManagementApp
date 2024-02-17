998db9677327f16f22bdc8f8dee9157cfba57a12
TankistPro <nakol.nayk@mail.ru>
2024-02-17 18:07:03 +0300

    Init

---
+++ FinancialManagementApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FinancialManagementApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
