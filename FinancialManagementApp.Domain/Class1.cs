cb3eac454a4511675308367071ccdd2702e5f573
TankistPro <nakol.nayk@mail.ru>
2024-02-17 18:43:02 +0300

    Добавлен Domain слой

---
+++ FinancialManagementApp.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagementApp.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ FinancialManagementApp.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8832be22-0caf-41db-9bfe-9d85aec9caa0")]
