ea3490c58b605ee868d98f5caf1de7489412fb11
TankistPro <nakol.nayk@mail.ru>
2024-02-26 23:44:36 +0300

    Добавлен шаблон графика

--- FinancialManagementApp/Pages/Home/MainPage.xaml.cs
+++ FinancialManagementApp/Pages/Home/MainPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using ScottPlot;
+
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace FinancialManagementApp.Pages
 {
@@ -23,6 +13,37 @@ namespace FinancialManagementApp.Pages
         public MainPage()
         {
             InitializeComponent();
+            this.Loaded += (s, e) => InitStatisticPlot();
+        }
+
+        private void InitStatisticPlot()
+        {
+            double[] values = { 778, 283, 184, 76, 43 };
+
+            PieSlice slice1 = new() { Value = 11204.80, FillColor = Colors.LightGray, Label = $"Расходы - {11204.80}руб." };
+            PieSlice slice2 = new() { Value = 13019.50, FillColor = Colors.LightGreen, Label = $"Доходы  - {13019.50}руб." };
+
+            slice1.LabelStyle.FontSize = 18;
+            slice2.LabelStyle.FontSize = 18;
+
+            slice1.LabelStyle.Bold = true;
+            slice2.LabelStyle.Bold = true;
+
+            slice1.LabelStyle.ForeColor = Colors.Gray;
+            slice2.LabelStyle.ForeColor = Colors.Green;
+
+            List<PieSlice> slices = new() { slice1, slice2 };
+
+            var pie = StatisticChart.Plot.Add.Pie(slices);
+            pie.ExplodeFraction = .04;
+            pie.ShowSliceLabels = true;
+
+            StatisticChart.Plot.ShowLegend(Alignment.LowerLeft);
+            StatisticChart.Plot.HideGrid();
+            StatisticChart.Plot.ScaleFactor = 1.5f;
+            StatisticChart.Plot.Layout.Frameless();
+            StatisticChart.Plot.Axes.AutoScale();
+            StatisticChart.Refresh();
         }
     }
 }
