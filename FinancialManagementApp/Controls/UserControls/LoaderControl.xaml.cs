a36e2c44b59e465cae6aa0cb307dbaac74c2fb63
TankistPro <nakol.nayk@mail.ru>
2024-03-03 22:02:32 +0300

    Loader вынесен в control

---
+++ FinancialManagementApp/Controls/UserControls/LoaderControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinancialManagementApp.Controls.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для LoaderControl.xaml
+    /// </summary>
+    public partial class LoaderControl : UserControl, INotifyPropertyChanged
+    {
+        public LoaderControl()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private string _loaderText;
+        public string LoaderText
+        {
+            get { return _loaderText; }
+            set
+            {
+                _loaderText = value;
+                OnPropertyChanged("LoaderText");
+            }
+        }
+    }
+}
